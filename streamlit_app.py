468e7537085d1a37a654610ae27b3f02cc983ee1
Ananya Saha <72212849+ananyasaha17@users.noreply.github.com>
2023-10-31 21:54:03 +0530

    Update streamlit_app.py
    error

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,6 @@ streamlit.dataframe(fruityvice_normalized)
 
 #dont run anything from here , we are having throubleshoot
 streamlit.stop()
-
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list ")
